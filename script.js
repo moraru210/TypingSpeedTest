d01effa0fae1dab17daa52f2ef868befba831147
moraru210 <alexandru210moraru@gmail.com>
2020-10-15 21:34:19 +0100

    I've added a wpm calculator, no backspace included

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+const stats = document.querySelector(".wpm");
 
 var timer = [0,0,0,0];
 var interval;
 var timerRunning = false;
+var numErrors = 0;
+var numGross = 0;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -27,10 +30,22 @@ function runTimer() {
   timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
+function calcWPM() {
+  let minutes = timer[0] + (timer[1]/60) + ((timer[2]/60)/100)
+  let currentWPM;
+  if ((numGross/5) > numErrors) {
+    currentWPM = ((numGross/5) - numErrors) / minutes;
+  } else {
+    currentWPM = 0;
+  }
+  stats.innerHTML = currentWPM.toFixed(0) + " wpm";
+}
+
 // Match the text entered with the provided text on the page:
 function spellCheck() {
   let textEntered = testArea.value;
   let originTextMatch = originText.substring(0, textEntered.length);
+  numGross++;
 
   if (textEntered == originText) {
     clearInterval(interval);
@@ -40,6 +55,7 @@ function spellCheck() {
       testWrapper.style.borderColor = "#65CCf3";
     } else {
       testWrapper.style.borderColor = "#E95D0F";
+      numErrors++;
     }
   }
   console.log(textEntered);
@@ -61,13 +77,17 @@ function reset() {
   interval = null;
   timer = [0,0,0,0];
   timerRunning = false;
+  numGross = 0;
+  numErrors = 0;
 
   testArea.value = "";
   theTimer.innerHTML = "00:00:00";
   testWrapper.style.borderColor = "grey";
+  stats.innerHTML = "0 wpm"
 }
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
+testArea.addEventListener("keypress", calcWPM, false);
 resetButton.addEventListener("click", reset, false);
